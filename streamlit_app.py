ed899ceaab5223cf37e35dd7e2a248857ba16065
Artur Batalini Alvarim <artur.batalini@gmail.com>
2026-01-08 10:57:23 -0300

    Update streamlit_app.py (requests library add)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -40,9 +41,10 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered! '+ name_on_order+ "!!", icon="✅")
-        
 
-    
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
 
